d525c75ebac652c896ffd03cd1e3d60aeda9e9d6
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2025-08-30 14:18:35 +0530

    routes setup

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcom to subscription tracker API ");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 const port = PORT || 5000;
 app.listen(port, () => {
