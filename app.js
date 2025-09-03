709ed3d4fb90c06c75d36c34b5e38c8c17e082fc
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2025-09-03 21:16:25 +0530

    Created error handler middleware

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcom to subscription tracker API ");
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Welcom to subscription tracker API ");
+});
 
 const port = PORT || 5000;
 
